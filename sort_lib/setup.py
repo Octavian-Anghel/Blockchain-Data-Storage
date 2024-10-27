3bce10b90a0de398cffa3de0a2667fe4109ac46a
thesnoopzer <157263321+thesnoopzer@users.noreply.github.com>
2024-10-27 19:21:12 -0400

    Update setup.py

--- sort_lib/setup.py
+++ sort_lib/setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sort-lib',
-    version='0.1.0',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},  # Tells setuptools to look in the src directory
-    install_requires=[
-        
-    ],
+    name="sort-lib",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},  # Tells setuptools to look in the src directory
+    install_requires=[],
 )
