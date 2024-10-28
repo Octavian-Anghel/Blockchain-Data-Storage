13f5f9c21f527694c419ab6c0710aefc1f590865
Ozwin Cordes <97770246+oz1x@users.noreply.github.com>
2024-10-28 14:39:34 -0400

    Update sorting_algorithms.py

--- sort_lib/sorting_algorithms.py
+++ sort_lib/sorting_algorithms.py
@@ -10,7 +10,7 @@ def bubble_sort(arr):
     if len(arr) < 0:
         raise ValueError("Array is of invalid size.")
     if type(arr) is not list:
-        raise TypeError("arr parameter is not a list".)
+        raise TypeError("arr parameter is not a list.")
     for i in range(len(arr)):
         for j in range(0, len(arr) - i - 1):
             if arr[j] > arr[j + 1]:
@@ -30,7 +30,7 @@ def quick_sort(arr):
     if len(arr) < 0:
         raise ValueError("Array is of invalid size.")
     if type(arr) is not list:
-        raise TypeError("arr parameter is not a list".)
+        raise TypeError("arr parameter is not a list.")
     if len(arr) <= 1:
         return arr
     pivot = arr[len(arr) // 2]
@@ -52,7 +52,7 @@ def insertion_sort(arr):
     if len(arr) < 0:
         raise ValueError("Array is of invalid size.")
     if type(arr) is not list:
-        raise TypeError("arr parameter is not a list".)
+        raise TypeError("arr parameter is not a list.")
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
